9afa237f0f5254ee99c843264690c5cf34e40ecd
mustahidhasan <mustahidhasan9@gmail.com>
2025-07-23 12:11:56 +0600

    Front end base is ready

--- Frontend/todo/src/App.js
+++ Frontend/todo/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './assets/css/App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Todo from './components/Todo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Navigation Bar */}
+        <nav className="navbar">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/todo">Todo</Link></li>
+          </ul>
+        </nav>
+
+        {/* Page Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/todo" element={<Todo />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
