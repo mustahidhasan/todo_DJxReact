524246fb35ba894f8b0d1ffb132ef0a1c3ec5516
mustahidhasan <mustahidhasan9@gmail.com>
2025-07-25 15:56:25 +0600

    register nav is not needed

--- Frontend/todo/src/App.js
+++ Frontend/todo/src/App.js
@@ -14,7 +14,6 @@ function App() {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Register</Link></li>
             <li><Link to="/todo">Todo</Link></li>
           </ul>
         </nav>
